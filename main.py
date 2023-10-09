aab32f3422d8a01c771f2421269f903168760ac0
Jonathan Smith <jondsmith22@gmail.com>
2023-10-09 10:04:55 -0500

    tuning / bug fix

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from PIL import Image, ImageGrab
-import time
+from PIL import Image, ImageGrab, ImageDraw
+import argparse
 import pyautogui
 import pydirectinput
 import sys
+import time
 import win32gui
 
 from screenshot_reader import ScreenshotReader
 import solving
+
+# TODO, would like to be able to solve from any current solvable state of the game
+# Right now the only reason that's not possible is
+#   the first ring having hardcoded specialness
+#   the selected key being assumed as key 0 in the grid.
+# Both are pretty easy fixes, just a bit of a chore for later
 
 # Return a PIL Image of Starfield
 def take_starfield_screenshot():
@@ -23,19 +30,60 @@ def take_starfield_screenshot():
         return ImageGrab.grab(dimensions)
     return pyautogui.screenshot()
 
-sf_pil = Image.open(sys.argv[1]) if len(sys.argv) > 1 else take_starfield_screenshot()
-reader = ScreenshotReader(sf_pil)
+parser = argparse.ArgumentParser(
+    prog='Starfield-Auto-Digipick',
+    description='Automatically solve the digipicking minigame',
+)
+
+parser.add_argument('-d', '--dry_run', action='store_true')
+parser.add_argument('-g', '--debug', action='store_true')
+parser.add_argument('-i', '--from-image')
+args = parser.parse_args(sys.argv[1:])
+
+t0 = time.time()
+
+sf_pil = Image.open(args.from_image) if args.from_image else take_starfield_screenshot()
+
+debug_pil = None
+debug_draw = None
+if args.debug:
+    debug_pil = sf_pil.copy()
+    debug_draw = ImageDraw.Draw(debug_pil)
+reader = ScreenshotReader(sf_pil, debug_draw)
+
+t1 = time.time()
+
 lock_rings = reader.read_lock()
+
+t2 = time.time()
+
 key_rings = reader.read_all_key_states()
-# TODO, would like to be able to solve from any current solvable state of the game
-# Right now the only reason that's not possible is
-#   the first ring having hardcoded specialness
-#   the selected key being assumed as key 0 in the grid.
-# Both are pretty easy fixes, just a bit of a chore for later
-reader.save_debug_image('screen.png', 'debug.png')
+
+t3 = time.time()
+
+if args.debug:
+    debug_pil.save('debug.png')
+    sf_pil.save('screen.png')
+
+t4 = time.time()
+
 moves = solving.solve(lock_rings, key_rings)
+
+t5 = time.time()
+
 control_sequence = solving.moves_to_keystrokes(moves, key_rings)
 print(f"Control sequence: {control_sequence}")
 
-if len(sys.argv) <= 1:
+t6 = time.time()
+
+print(f"""Timing summary (milliseconds))
+    screenshot prep:    {int(1000*(t1-t0))}
+    reading lock:       {int(1000*(t2-t1))}
+    reading keys:       {int(1000*(t3-t2))}
+    saving images:      {int(1000*(t4-t3))}
+    game solving:       {int(1000*(t5-t4))}
+    control sequencing: {int(1000*(t6-t5))}
+""")
+
+if not args.dry_run:
     pydirectinput.typewrite(control_sequence)
--- screenshot_reader.py
+++ screenshot_reader.py
@@ -1,29 +1,23 @@
 import math
-import pyautogui
 import statistics
-from PIL import Image, ImageDraw
 
 # Value guesses
 height_to_lock_radius = 0.3785 / 2.0
-lock_radius_to_key_distance_x = 0.555 # The horizontal distance between columns of keys
-lock_radius_to_key_distance_y = 0.544 # The vertical distance between rows of keys
-lock_radius_to_key_radius = 0.185 # The radius of a key ring
+lock_radius_to_key_distance_x = 0.553 # The horizontal distance between columns of keys
+lock_radius_to_key_distance_y = 0.543 # The vertical distance between rows of keys
+lock_radius_to_key_radius = 0.186 # The radius of a key ring
 lock_ring_stride = 0.165 # The distance between individual lock rings
+key_search_grid_stride = 0.1 # The size of the grid step when looking for the selected key
 
 class ScreenshotReader(object):
 
-    def __init__(self, im):
+    def __init__(self, im, draw=None):
         self.im = im
         self.px = self.im.load()
-        self.test_output_im = self.im.copy()
-        self.draw = ImageDraw.Draw(self.test_output_im)
+        self.draw = draw
         self.lock_radius_px = self.im.height * height_to_lock_radius
         self.key_distance_x = self.lock_radius_px * lock_radius_to_key_distance_x
         self.key_distance_y = self.lock_radius_px * lock_radius_to_key_distance_y
-
-    def save_debug_image(self, filename, annotated_filename):
-        self.test_output_im.save(annotated_filename)
-        self.im.save(filename)
 
     # Given an x,y coordinate in the image,
     # Return the subjective brightness value there, where out-of-image pixels are assumed to be black
@@ -51,10 +45,13 @@ class ScreenshotReader(object):
                         brightnesses.append(self.brightness(x+dx, y+dy))
                 if statistics.stdev(brightnesses) < threshold:
                     lock_rings[ring_number] |= (1 << rotation_number)
-                    self.draw.ellipse((x-10, y-10, x+10, y+10), fill = None, outline ='green')
+                    if self.draw:
+                        self.draw.ellipse((x-10, y-10, x+10, y+10), fill = None, outline ='green')
                 else:
-                    self.draw.ellipse((x-10, y-10, x+10, y+10), fill = None, outline ='red')
-                self.draw.text((x,y), f"{rotation_number}", fill=(255, 255, 0))
+                    if self.draw:
+                        self.draw.ellipse((x-10, y-10, x+10, y+10), fill = None, outline ='red')
+                if self.draw:
+                    self.draw.text((x,y), f"{rotation_number}", fill=(255, 255, 0))
             if lock_rings[ring_number] == 0xffffffff:
                 lock_rings = lock_rings[:ring_number]
                 break
@@ -71,15 +68,16 @@ class ScreenshotReader(object):
         max_x = int(self.im.width*.95)
         min_y = int(self.im.height*.25)
         max_y = int(self.im.height*.75)
-        self.draw.line([(min_x,0),(min_x,self.im.height)], fill='cyan')
-        self.draw.line([(max_x,0),(max_x,self.im.height)], fill='cyan')
-        self.draw.line([(0,min_y),(self.im.width,min_y)], fill='cyan')
-        self.draw.line([(0,max_y),(self.im.width,max_y)], fill='cyan')
+        if self.draw:
+            self.draw.line([(min_x,0),(min_x,self.im.height)], fill='cyan')
+            self.draw.line([(max_x,0),(max_x,self.im.height)], fill='cyan')
+            self.draw.line([(0,min_y),(self.im.width,min_y)], fill='cyan')
+            self.draw.line([(0,max_y),(self.im.width,max_y)], fill='cyan')
 
         # Walk through the region where the keys might be with a stride of 10px
         # in both directions and gather the average and stdev of the brightness
         brightnesses = []
-        grid_step = 20 # TODO sense resolution of image and adjust
+        grid_step = int(self.im.height * height_to_lock_radius * key_search_grid_stride)
         for x in range(min_x, max_x, grid_step):
             for y in range(min_y, max_y, grid_step):
                 brightnesses.append(self.brightness(x, y))
@@ -116,15 +114,18 @@ class ScreenshotReader(object):
             radius_stdev = statistics.stdev(radius_measurements)
             debug_info.append([cx, cy, center_x, center_y, radius, radius_stdev])
             if radius_stdev > 2.0: # TODO this fudge factor is arbitrary
-                self.draw.ellipse((cx-10, cy-10, cx+10, cy+10), fill = None, outline ='red')
+                if self.draw:
+                    self.draw.ellipse((cx-10, cy-10, cx+10, cy+10), fill = None, outline ='red')
                 continue
             else:
-                self.draw.ellipse((cx-10, cy-10, cx+10, cy+10), fill = None, outline ='green')
+                if self.draw:
+                    self.draw.ellipse((cx-10, cy-10, cx+10, cy+10), fill = None, outline ='green')
             if radius > max_radius:
                 best_point = (center_x, center_y)
                 max_radius = radius
         fkx, fky = best_point
-        self.draw.ellipse((fkx-10, fky-10, fkx+10, fky+10), fill = None, outline ='blue')
+        if self.draw:
+            self.draw.ellipse((fkx-10, fky-10, fkx+10, fky+10), fill = None, outline ='blue')
         return best_point
 
     def key_at(self, key_x, key_y):
@@ -134,20 +135,23 @@ class ScreenshotReader(object):
             theta = 2*math.pi*(((rotation_number - 8) % 32) / 32.0)
             (x0, y0) = (r*math.cos(theta), r*math.sin(theta))
             (x, y) = x0+key_x, y0+key_y
-            self.draw.ellipse((x-1, y-1, x+1, y+1), fill = 'yellow', outline ='yellow')
+            if self.draw:
+                self.draw.ellipse((x-1, y-1, x+1, y+1), fill = 'yellow', outline ='yellow')
             brightnesses.append(self.brightness(x, y))
         if statistics.stdev(brightnesses) < 0.07: # arbitrary threshold
             return 0
         mean_brightness = statistics.mean(brightnesses)
+        stdev_brightness = statistics.stdev(brightnesses)
         key = 0
         for rotation_number in range(32):
             r = lock_radius_to_key_radius * self.im.height * height_to_lock_radius
             theta = 2*math.pi*(((rotation_number - 8) % 32) / 32.0)
             (x0, y0) = (r*math.cos(theta), r*math.sin(theta))
             (x, y) = x0+key_x, y0+key_y
-            if brightnesses[rotation_number] > mean_brightness:
+            if brightnesses[rotation_number] > mean_brightness + stdev_brightness:
                 key |= (1 << rotation_number)
-                self.draw.line([(key_x,key_y),(x,y)], fill='cyan')
+                if self.draw:
+                    self.draw.line([(key_x,key_y),(x,y)], fill='cyan')
         return key
 
     def read_all_key_states(self):
@@ -157,7 +161,8 @@ class ScreenshotReader(object):
             for col_num in range(4):
                 x, y = (first_key_center[0] + int(col_num*self.key_distance_x),
                         first_key_center[1] + int(row_num*self.key_distance_y))
-                self.draw.ellipse((x-5, y-5, x+5, y+5), fill = 'purple', outline ='purple')
+                if self.draw:
+                    self.draw.ellipse((x-5, y-5, x+5, y+5), fill = 'purple', outline ='purple')
                 result.append(self.key_at(x, y))
         return result
 
